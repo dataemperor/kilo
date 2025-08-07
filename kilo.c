1e8c4d83fc45e858eff49364ea5aa6ae165de723
Thantulage Jayathu Chankama Fernando <155064611+dataemperor@users.noreply.github.com>
2025-08-07 19:39:56 +0530

    Turning off echoing

--- kilo.c
+++ kilo.c
@@ -1,6 +1,19 @@
+#include <termios.h>
 #include <unistd.h>
 
+void enableRawMode() {
+    struct termios raw;
+
+    tcgetattr(STDIN_FILENO, &raw);
+
+    raw.c_lflag &= ~(ECHO);
+
+    tcsetattr(STDERR_FILENO, TCIFLUSH, &raw);
+}
+
 int main() {
+    enableRawMode();
+
     char c;
     while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
     return 0;
